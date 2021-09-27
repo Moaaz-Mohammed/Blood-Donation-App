b83d8469a1cb7c50570586cfbd8b09c07677dd4e
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-27 14:54:32 +0200

    edit app structure

--- lib/Screens/patient_page.dart
+++ lib/Screens/patient_page.dart
@@ -1,3 +1,4 @@
+import 'package:blood_donation/Widgets/PatientSubmitButton.dart';
 import 'package:blood_donation/shared/Constants.dart';
 import 'package:blood_donation/shared/Controllers.dart';
 import 'package:blood_donation/shared/Cubit/Cubit.dart';
@@ -374,49 +375,7 @@ class _PatientState extends State<Patient> {
                                 height: 40,
                               ),
                               //Submit Button
-                              Container(
-                                height: 60,
-                                width: 300,
-                                decoration: BoxDecoration(
-                                  color: Constants.redColor,
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: TextButton(
-                                  onPressed: (){
-                                    if (formKey.currentState!.validate()) {
-                                      final firestoreInstance =
-                                          FirebaseFirestore.instance;
-                                      firestoreInstance.collection("Patient").add({
-                                        "name": nameController.text,
-                                        "status": statusController.text,
-                                        "phone": phoneController.text,
-                                        "another_phone": anotherphoneController.text,
-                                        "blood_type": BloodType.toString(),
-                                        "age": ageController.text,
-                                        "hospital": hospitalController.text,
-                                        "blood_needed": bloodamountController.text,
-                                        "registered_time": DateTime.now().toLocal(),
-                                      }).then(
-                                            (value) => showDialog(
-                                            context: context,
-                                            builder: (context) => CustomDialog(
-                                              image: Image.network('https://media.tenor.com/images/2525ba72ea472c55bf8b2153e9ea9f11/tenor.gif'),
-                                              title: 'تم إرسال البيانات بنجاح',
-                                              description1:
-                                              'تم إرسال البيانات وسيتم التواصل معك في أقرب فرصة',
-                                              description2: '',
-                                            )).then((value) =>
-                                            navigateTo(context, home_page())),
-                                      );
-                                    };
-                                  },
-                                  child: Text(
-                                    'إرسـال البيـانـات',
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold, fontSize: 22, color: Colors.white),
-                                  ),
-                                ),
-                              ),
+                              PatientSubmitButton(),
                             ],
                           ),
                         ),
