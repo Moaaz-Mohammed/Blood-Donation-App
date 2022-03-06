2dee94619313c429ac780b0a67619c9d714d6b34
ahmed <a7md.s3d.2001@gmail.com>
2022-03-06 22:27:53 +0200

    00

--- lib/screens/auth/reset_password.dart
+++ lib/screens/auth/reset_password.dart
@@ -21,6 +21,8 @@ class ResetPasswordScreen extends StatefulWidget {
 
 class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   TextEditingController emailController = TextEditingController();
+  GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -32,68 +34,79 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: Constants.primaryPadding,
-          child: Center(
-            child: Column(
-              children: [
-                Image.asset(
-                  Images.resetPasswordImage,
-                  width: width * 0.6,
-                ),
-                const CustomSizedBoxHeight(),
-                const CustomSizedBoxHeight(),
-                Text(
-                  LocaleKeys.reset_password.tr(),
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-                const CustomSizedBoxHeight(),
-                Text(
-                  LocaleKeys.reset_password_content.tr(),
-                  style: Theme.of(context).textTheme.headline1?.copyWith(
-                      color: AppCubit.get(context).isDark
-                          ? CustomColors.primaryDarkColor.withOpacity(0.5)
-                          : CustomColors.primaryRedColor),
-                ),
-                const CustomSizedBoxHeight(),
-                const CustomSizedBoxHeight(),
-                const CustomSizedBoxHeight(),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  style: Theme.of(context).textTheme.headline2,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.mail),
-                    hintText: LocaleKeys.email.tr(),
+          child: Form(
+            key: _globalKey,
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset(
+                    Images.resetPasswordImage,
+                    width: width * 0.6,
                   ),
-                ),
-                SizedBox(
-                  height: height * 0.2,
-                ),
-                CustomButton(
-                    onTap: () async {
-                      try {
-                        await FirebaseAuth.instance
-                            .sendPasswordResetEmail(
-                                email: emailController.text.trim())
-                            .then((value) {
-                          Functions.showToastMsg(
-                            title: LocaleKeys.sent.tr(),
-                          ).then((value) {
-                            Functions.showToastMsg(
-                              title: LocaleKeys.check_your_email.tr(),
-                            );
-                          }).then((value) {
-                            Functions.navigatorPush(
-                              context: context,
-                              screen: const LogInScreen(),
-                            );
-                          });
-                        });
-                      } on FirebaseAuthException catch (error) {
-                        Functions.showToastMsg(title: error.toString());
+                  const CustomSizedBoxHeight(),
+                  const CustomSizedBoxHeight(),
+                  Text(
+                    LocaleKeys.reset_password.tr(),
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  const CustomSizedBoxHeight(),
+                  Text(
+                    LocaleKeys.reset_password_content.tr(),
+                    style: Theme.of(context).textTheme.headline1?.copyWith(
+                        color: AppCubit.get(context).isDark
+                            ? CustomColors.primaryDarkColor.withOpacity(0.5)
+                            : CustomColors.primaryRedColor),
+                  ),
+                  const CustomSizedBoxHeight(),
+                  const CustomSizedBoxHeight(),
+                  const CustomSizedBoxHeight(),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    style: Theme.of(context).textTheme.headline2,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.mail),
+                      hintText: LocaleKeys.email.tr(),
+                    ),
+                    validator: (value){
+                      if  (value!.isEmpty){
+                        return 'required';
                       }
+                      return null;
                     },
-                    title: LocaleKeys.reset_password.tr())
-              ],
+                  ),
+                  SizedBox(
+                    height: height * 0.2,
+                  ),
+                  CustomButton(
+                      onTap: () async {
+                        if(_globalKey.currentState!.validate()){
+                          try {
+                            await FirebaseAuth.instance
+                                .sendPasswordResetEmail(
+                                email: emailController.text.trim())
+                                .then((value) {
+                              Functions.showToastMsg(
+                                title: LocaleKeys.sent.tr(),
+                              ).then((value) {
+                                Functions.showToastMsg(
+                                  title: LocaleKeys.check_your_email.tr(),
+                                );
+                              }).then((value) {
+                                Functions.navigatorPush(
+                                  context: context,
+                                  screen: const LogInScreen(),
+                                );
+                              });
+                            });
+                          } on FirebaseAuthException catch (error) {
+                            Functions.showToastMsg(title: error.toString());
+                          }
+                        }
+                      },
+                      title: LocaleKeys.reset_password.tr())
+                ],
+              ),
             ),
           ),
         ),
